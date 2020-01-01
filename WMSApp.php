5a4c9a181c6f63dfc04a442a1484f438c4ef1186
Andrej Kabachnik <andrej_ka@gmx.de>
2020-01-01 23:50:35 +0100

    FIX SQL installer in app

--- WMSApp.php
+++ WMSApp.php
@@ -11,14 +11,14 @@ class WMSApp extends App
 {
     public function getInstaller(InstallerInterface $injected_installer = null)
     {
-        $installer = parent::getInstaller($injected_installer);        
+        $installer = parent::getInstaller($injected_installer); 
+        
         $schema_installer = new MySqlDatabaseInstaller($this->getSelector());
         $schema_installer
         ->setDataSourceSelector('0x11e83d69112063269e40e4b318306b9a')
         ->setFoldersWithMigrations(['InitDB', 'DemoData']);
         $installer->addInstaller($schema_installer);
         
-        $schema_installer->getDataConnection();
         return $installer;
     }
 }
